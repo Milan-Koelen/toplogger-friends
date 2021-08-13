e537ae6bb3fb8467247d357a0338d17593f0aada
Milan-Koelen <67652887+Milan-Koelen@users.noreply.github.com>
2021-08-14 00:54:05 +0200

    first share

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, useRef } from "react";
+import "./App.css";
+
+const svgUrl =
+  "https://cdn1.toplogger.nu/images/gyms/monk_eindhoven/floorplan.svg?5";
+
+const x = 300;
+const y = 0;
 
 function App() {
+  const svgContainerRef = useRef(null);
+
+  const parseToploggerSVG = svgText => {
+    if (!svgContainerRef.current) return;
+    var parser = new DOMParser();
+    var doc = parser.parseFromString(svgText, "image/svg+xml");
+
+    var newSVG = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    var svgNS = newSVG.namespaceURI;
+
+    var moveGroup = document.createElementNS(svgNS, "g");
+    moveGroup.setAttribute("transform", `translate(${x},${y}) scale(1)`);
+    newSVG.appendChild(moveGroup);
+
+    var scaleGroup = doc.getElementById("zoom_layer");
+    scaleGroup.setAttribute("transform", "translate(0,0) scale(0.1)");
+    moveGroup.appendChild(scaleGroup);
+
+    // console.log(doc);
+
+    svgContainerRef.current.innerHtml = "";
+    svgContainerRef.current.appendChild(newSVG);
+
+    return doc;
+  };
+
+  useEffect(() => {
+    fetch(svgUrl)
+      .then(res => res.text())
+      .then(data => parseToploggerSVG(data));
+  }, [svgContainerRef]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"></header>
+      <div className="floorplan" ref={svgContainerRef}></div>
     </div>
   );
 }
