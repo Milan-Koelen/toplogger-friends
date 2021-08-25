466e1c163b2d13d4a2cfc459aad2191fa182c826
Milan-Koelen <67652887+Milan-Koelen@users.noreply.github.com>
2021-08-25 16:48:13 +0200

    Search

--- src/_components/Search.js
+++ src/_components/Search.js
@@ -1,40 +1,57 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React, { useState } from "react";
+// import { useDispatch, useSelector } from "react-redux";
 import Search from "./Search";
 
 const SearchUser = () => {
-  // const [name, setName] = useState("");
   const [username, setUsername] = useState("");
 
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
 
   const handleSearch = e => {
     e.preventDefault();
 
-    dispatch(
-      Search({
-        username: username,
-      })
-    );
+    // search function to api
 
-    setUsername("");
-  };
+    const search = username => {
+      return async dispatch => {
+        try {
+          const result = await fetch(URL + "/search", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: { username: username }, // body data type must match "Content-Type" header
+          });
 
-  return (
-    <div className="search">
-      <h1>Search</h1>
-      <div className="search__form">
-        <input
-          type="username"
-          placeholder="Toplogger Username"
-          value={username}
-          onChange={e => setUsername(e.target.value)}
-        ></input>
-        <button className="search__btn" onClick={handleSearch}>
-          Register
-        </button>
+          if (result.status !== 200) {
+            console.error(await result.text(), result.status);
+            return;
+          }
+
+          const data = await result.json();
+          console.log(data);
+        } catch (e) {
+          console.error(e);
+        }
+      };
+    };
+
+    return (
+      <div className="search">
+        <h1>Search</h1>
+        <div className="search__form">
+          <input
+            type="username"
+            placeholder="Toplogger Username"
+            value={username}
+            onChange={e => setUsername(e.target.value)}
+          ></input>
+          <button className="search__btn" onClick={handleSearch(username)}>
+            Register
+          </button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  };
 };
 export default SearchUser;
