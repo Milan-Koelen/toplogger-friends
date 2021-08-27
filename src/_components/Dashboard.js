0953cacc5a81040a4040999285425609cd8d0963
Milan-Koelen <67652887+Milan-Koelen@users.noreply.github.com>
2021-08-27 18:08:30 +0200

    dashboard

--- src/_components/Dashboard.js
+++ src/_components/Dashboard.js
@@ -5,13 +5,30 @@ import { logout, selectUser } from "../features/userSlice";
 import { fetchFollowing } from "../features/followingSlice";
 import "./Dashboard.css";
 
-import { VictoryBar, VictoryChart } from "victory";
+// import { VictoryBar, VictoryChart } from "victory";
 
 const data = [
-  { quarter: 1, earnings: 13000 },
-  { quarter: 2, earnings: 16500 },
-  { quarter: 3, earnings: 14250 },
-  { quarter: 4, earnings: 19000 },
+  {
+    Name: "Nick Remijn",
+    TL_ID: 27026,
+    Grade: 5000,
+    ProfilePictureURL:
+      "https://lh3.googleusercontent.com/a/AATXAJzvXVrgZMmzRwLWiMzMXk4mbqb2Xp1FRWdPM25N=s50-mo",
+  },
+  {
+    Name: "Willem Kluskens",
+    TL_ID: 14345,
+    Grade: 3000,
+    ProfilePictureURL:
+      "https://upload.toplogger.nu/avatars/744008245460804119811370344438458001.png",
+  },
+  {
+    Name: "Stijn van Lierop",
+    TL_ID: 130188,
+    Grade: 1200,
+    ProfilePictureURL:
+      "https://lh3.googleusercontent.com/a-/AOh14GgGmqMlVYVbW6iACP4sxWJbVKN30ln21kJI2aHx=s50",
+  },
 ];
 
 const Dashboard = () => {
@@ -29,14 +46,48 @@ const Dashboard = () => {
   }, [dispatch, user.token, user.name, user.TL_Grade]);
   return (
     <div className="logout">
-      <div className="chart">
+      {/* <div className="chart">
         <VictoryChart>
           <VictoryBar data={data} x="quarter" y="earnings" />
         </VictoryChart>
-      </div>
-      <h1>
+      </div> */}
+      <h1 className="welcome_user">
         Welcome <span className="user__name">{user.name}</span>
       </h1>
+
+      <div>
+        <h2 className="title">Leaderboard</h2>
+        <table className="leaderboard">
+          <tr>
+            <th>Picture</th>
+            <th>name</th>
+            <th>grade</th>
+          </tr>
+          <tbody>
+            {data.map((i, idx) => (
+              <tr>
+                <td>
+                  <img
+                    className="profilepicture_lb"
+                    src={i.ProfilePictureURL}
+                    alt={i.Name}
+                  ></img>
+                </td>
+                <td>
+                  <a href={URL + "/user?TL_ID=" + i.TL_ID} className="name">
+                    {i.Name}{" "}
+                  </a>
+                </td>
+                <td>
+                  <span className="grade">{i.Grade} </span>
+                </td>
+                <br />
+                <br />
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
       <button className="logout__btn" onClick={e => handleLogout(e)}>
         Logout
       </button>
