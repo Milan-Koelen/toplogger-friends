101574222093412d091c5d542e803b10b2902c19
Milan-Koelen <67652887+Milan-Koelen@users.noreply.github.com>
2021-08-28 16:46:44 +0200

    Search link

--- src/_components/Layout.js
+++ src/_components/Layout.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { HOME_URL } from "../config";
 import "./Layout.css";
+import { Link } from "react-router-dom";
 
 export default function Layout({ children }) {
   return (
@@ -8,6 +9,7 @@ export default function Layout({ children }) {
       <div className="header">
         <a href={HOME_URL}>
           <h3> TopLogger Friends</h3>
+          <Link to="/search"> Search</Link>
         </a>
       </div>
       {children}
