0852feb2f062b91229f6a11148d089f14bedad24
Milan-Koelen <67652887+Milan-Koelen@users.noreply.github.com>
2021-09-03 16:25:44 +0200

    Change tab name and clean index.js/html

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
-import store from "./app/store";
 import { BrowserRouter as Router } from "react-router-dom";
-import { PersistGate } from "redux-persist/integration/react";
-
 import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+import App from "./App";
+import store from "./app/store";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
 let persistor = persistStore(store);
 
