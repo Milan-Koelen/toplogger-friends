f7d5f0edbfbba66d0598676d8d16d7bb3c285ea4
Milan-Koelen <67652887+Milan-Koelen@users.noreply.github.com>
2021-10-04 16:05:03 +0200

    background login

--- src/_pages/Login.js
+++ src/_pages/Login.js
@@ -8,6 +8,7 @@ import {
   TextField,
 } from "@material-ui/core";
 import React, { useState } from "react";
+import Background from "../components/background.js";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router";
 import { login, selectUser, signup } from "../features/userSlice";
@@ -130,6 +131,7 @@ const Login = () => {
         </Paper>
       </Grid>
     </div>
+<Background></Background>
   );
 };
 export default Login;
