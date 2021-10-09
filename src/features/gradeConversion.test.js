4ce6ac369e0ac8cb3d56c68d5064113160b3dac3
Nick Remijn <nick.remijn@gmail.com>
2021-10-09 16:58:13 +0200

    fix grade percentage (behalve tim)

--- src/features/gradeConversion.test.js
+++ src/features/gradeConversion.test.js
@@ -1,9 +1,15 @@
 import convertGrade from "./gradeConversion";
 
 test("convert grades", () => {
-  expect(convertGrade(3.2)).toBe("3a");
-  expect(convertGrade(4.2)).toBe("4a");
-  expect(convertGrade(6.1)).toBe("6a");
-  expect(convertGrade(6.25)).toBe("6a+");
-  expect(convertGrade(11)).toBe("9b+");
+  expect(convertGrade(3.2)[0]).toBe("3");
+  expect(convertGrade(4.2)[0]).toBe("4");
+  expect(convertGrade(6.1)[0]).toBe("6A");
+  expect(convertGrade(6.25)[0]).toBe("6A+");
+  expect(convertGrade(11)[0]).toBe("9B+");
+});
+
+test("check percentages", () => {
+  expect(convertGrade(6.78334)[1]).toBe(69);
+  expect(convertGrade(6.35069)[1]).toBe(12);
+  expect(convertGrade(7.03301)[1]).toBe(18);
 });
