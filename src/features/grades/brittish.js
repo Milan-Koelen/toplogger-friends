d5ad4a29559a5ddf9e7d747842bfcea859a8472b
Nick Remijn <nick.remijn@gmail.com>
2021-10-09 16:05:59 +0200

    grades

---
+++ src/features/grades/brittish.js
@@ -0,0 +1,27 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  key: "british",
+  name: "British",
+  description: "British Peak bouldering grade system",
+  types: ["boulder"],
+  data: [
+    { value: 2, name: "B0" },
+    { value: 3, name: "B1" },
+    { value: 3.83, name: "B2" },
+    { value: 5, name: "B3" },
+    { value: 6, name: "B4" }, // Compare-reference grade
+    { value: 6.28, name: "B5" },
+    { value: 6.56, name: "B6" },
+    { value: 6.83, name: "B7" },
+    { value: 7.11, name: "B8" },
+    { value: 7.39, name: "B9" },
+    { value: 7.67, name: "B10" }, // Compare-reference grade
+    { value: 7.87, name: "B11" },
+    { value: 8.08, name: "B12" },
+    { value: 8.29, name: "B13" },
+    { value: 8.5, name: "B14" }, // Compare-reference grade
+    { value: 8.67, name: "B15" },
+    { value: 8.83, name: "B16" },
+    { value: 9, name: "B17" },
+  ],
+};
